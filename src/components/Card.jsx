cad733f204c2efcbab2c74a337118a9d2fa81c8a
thempthename <noname03921@gmail.com>
2024-03-31 14:45:06 +0500

    crud posts

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,181 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "../App.css";
 import { useQuery } from "react-query";
-import "./Modal.css";
 
-function Card({ data, isOpen }) {
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+function Card() {
+  const [text, setText] = useState("");
+  const [link, setLink] = useState("");
+
+  const { data, isLoading } = useQuery(
+    "navlinks",
+    () => {
+      return axios.get(
+        "https://65e6e0fb53d564627a8d390f.mockapi.io/api/v1/nav-links"
+      );
+    },
+    {
+      refetchInterval: 500,
+    }
+  );
+
+  const handleEditNavLink = (e, id) => {
+    e.preventDefault();
+
+    const navLink = {
+      text: text,
+      link: link,
+    };
+
+    axios.put(
+      `https://65e6e0fb53d564627a8d390f.mockapi.io/api/v1/nav-links/${id}`,
+      navLink
+    );
+  };
+
+  const handleNewNavLink = function (e) {
+    e.preventDefault();
+
+    const navLink = {
+      text: text,
+      link: link,
+    };
+
+    axios.post(
+      `https://65e6e0fb53d564627a8d390f.mockapi.io/api/v1/nav-links/`,
+      navLink
+    );
+  };
+
+  const handleDeleteNavLink = function (id) {
+    axios.delete(
+      `https://65e6e0fb53d564627a8d390f.mockapi.io/api/v1/nav-links/${id}`
+    );
+  };
+
+  if (isLoading)
+    return (
+      <>
+        <h1>Loading...</h1>
+      </>
+    );
+
   return (
     <>
-      {isOpen && (
-        <div className="card-overlay">
-          <div className="card"></div>
-        </div>
-      )}
+      <div className="cards flex flex-col gap-2 p-3 border-2 border-orange-500 w-full ">
+        <h1 className="text-center text-3xl font-bold">Nav links</h1>
+        <Dialog>
+          <DialogTrigger>New +</DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Adding navlink</DialogTitle>
+            </DialogHeader>
+            <form
+              onSubmit={handleNewNavLink}
+              className="p-5 gap-5 flex flex-col border-2 rounded-3xl "
+            >
+              <div className="name flex flex-col gap-2">
+                <p className="ml-1 text-left">Text</p>
+                <input
+                  className="p-2 border-1 rounded-3xl w-full"
+                  type="text"
+                  placeholder="text"
+                  onChange={(e) => {
+                    setText(e.target.value);
+                  }}
+                />
+              </div>
+              <div className="link flex flex-col gap-2">
+                <p className="ml-1 text-left">Link</p>
+                <input
+                  className="p-2 border-1 rounded-3xl"
+                  type="text"
+                  placeholder="link"
+                  onChange={(e) => {
+                    setLink(e.target.value);
+                  }}
+                />
+              </div>
+              <button type="submit">Submit</button>
+            </form>
+          </DialogContent>
+        </Dialog>
+
+        {data.data.map((res, i) => {
+          return (
+            <div
+              key={i}
+              className="nav flex gap-2 p-3 border-2 rounded-3xl justify-between  "
+            >
+              <p className="">{res.text}</p>
+              <p>{res.link}</p>
+              <button
+                onClick={() => {
+                  handleDeleteNavLink(res.id);
+                }}
+              >
+                Delete
+              </button>
+              <Dialog>
+                <DialogTrigger
+                  onClick={() => {
+                    setText(res.text);
+                    setLink(res.link);
+                  }}
+                >
+                  Edit
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>Editing navlink with id {res.id}</DialogTitle>
+                  </DialogHeader>
+                  <form
+                    onSubmit={(e) => {
+                      handleEditNavLink(e, res.id);
+                    }}
+                    className="p-5 gap-5 flex flex-col border-2 rounded-3xl "
+                  >
+                    <div className="name flex flex-col gap-2">
+                      <p className="ml-1 text-left">Text</p>
+                      <input
+                        className="p-2 border-1 rounded-3xl w-full"
+                        defaultValue={res.text}
+                        type="text"
+                        placeholder="text"
+                        onChange={(e) => {
+                          setText(e.target.value);
+                        }}
+                      />
+                    </div>
+                    <div className="link flex flex-col gap-2">
+                      <p className="ml-1 text-left">Link</p>
+                      <input
+                        className="p-2 border-1 rounded-3xl"
+                        defaultValue={res.link}
+                        type="text"
+                        placeholder="link"
+                        onChange={(e) => {
+                          setLink(e.target.value);
+                        }}
+                      />
+                    </div>
+                    <button type="submit">Submit</button>
+                  </form>
+                </DialogContent>
+              </Dialog>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
---
+++ src/components/Posts.jsx
@@ -0,0 +1,234 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "../App.css";
+import { useQuery } from "react-query";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+function Posts() {
+  const [poster, setPoster] = useState("");
+  const [header, setHeader] = useState("");
+  const [description, setDescription] = useState("");
+  const [linkp, setLinkp] = useState("");
+
+  const { data, isLoading } = useQuery(
+    "posts",
+    () => {
+      return axios.get(
+        "https://65e6e0fb53d564627a8d390f.mockapi.io/api/v1/posts/"
+      );
+    },
+    {
+      refetchInterval: 500,
+    }
+  );
+
+  const handleEditPoster = (e, id) => {
+    e.preventDefault();
+
+    const post = {
+      poster: poster,
+      header: header,
+      description: description,
+      link: linkp,
+    };
+
+    axios.put(
+      `https://65e6e0fb53d564627a8d390f.mockapi.io/api/v1/posts/${id}`,
+      post
+    );
+  };
+
+  const handleNewPoster = function (e) {
+    e.preventDefault();
+
+    const post = {
+      poster: poster,
+      header: header,
+      description: description,
+      link: linkp,
+    };
+
+    axios.post(
+      `https://65e6e0fb53d564627a8d390f.mockapi.io/api/v1/posts/`,
+      post
+    );
+  };
+
+  const handleDeletePoster = function (id) {
+    axios.delete(
+      `https://65e6e0fb53d564627a8d390f.mockapi.io/api/v1/posts/${id}`
+    );
+  };
+
+  if (isLoading)
+    return (
+      <>
+        <h1>Loading...</h1>
+      </>
+    );
+  return (
+    <>
+      <div className="posters flex flex-col gap-2 p-3 border-2 border-orange-500 w-full ">
+        <h1 className="text-center text-3xl font-bold">Posters</h1>
+        <Dialog>
+          <DialogTrigger>New +</DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Adding posters</DialogTitle>
+              <br />
+              <form
+                onSubmit={handleNewPoster}
+                className="p-5 gap-5 flex flex-col border-2 rounded-3xl "
+              >
+                <div className="poster  flex flex-col gap-2">
+                  <p className="ml-1 text-left">Poster</p>
+                  <input
+                    className="p-2 border-1 rounded-3xl w-full"
+                    type="text"
+                    placeholder="text"
+                    onChange={(e) => {
+                      setPoster(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="header">
+                  <p className="ml-1 text-left">Header</p>
+                  <input
+                    className="p-2 border-1 rounded-3xl w-full"
+                    type="text"
+                    placeholder="text"
+                    onChange={(e) => {
+                      setHeader(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="description">
+                  <p className="ml-1 text-left">Description</p>
+                  <input
+                    className="p-2 border-1 rounded-3xl w-full"
+                    type="text"
+                    placeholder="text"
+                    onChange={(e) => {
+                      setDescription(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="link">
+                  <p className="ml-1 text-left">Link</p>
+                  <input
+                    className="p-2 border-1 rounded-3xl w-full"
+                    type="text"
+                    placeholder="text"
+                    onChange={(e) => {
+                      setLinkp(e.target.value);
+                    }}
+                  />
+                </div>
+                <button type="submit">Submit</button>
+              </form>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+        {data.data.map((res, i) => {
+          return (
+            <div
+              key={i}
+              className="nav flex gap-2 p-3 border-2 rounded-3xl justify-between"
+            >
+              <p>{res.poster}</p>
+              <p>{res.header}</p>
+              <p>{res.description}</p>
+              <p>{res.link}</p>
+              <button
+                onClick={() => {
+                  handleDeletePoster(res.id);
+                }}
+              >
+                Delete
+              </button>
+              <Dialog>
+                <DialogTrigger
+                  onClick={() => {
+                    setPoster(res.poster);
+                    setHeader(res.header);
+                    setDescription(res.description);
+                    setLinkp(res.link);
+                  }}
+                >
+                  Edit
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>Editing post with id {res.id}</DialogTitle>
+                  </DialogHeader>
+                  <form
+                    onSubmit={(e) => {
+                      handleEditPoster(e, res.id);
+                    }}
+                    className="p-5 gap-5 flex flex-col border-2 rounded-3xl "
+                  >
+                    <div className="poster  flex flex-col gap-2">
+                      <p className="ml-1 text-left">Poster</p>
+                      <input
+                        className="p-2 border-1 rounded-3xl w-full"
+                        type="text"
+                        placeholder="text"
+                        onChange={(e) => {
+                          setPoster(e.target.value);
+                        }}
+                      />
+                    </div>
+                    <div className="header">
+                      <p className="ml-1 text-left">Header</p>
+                      <input
+                        className="p-2 border-1 rounded-3xl w-full"
+                        type="text"
+                        placeholder="text"
+                        onChange={(e) => {
+                          setHeader(e.target.value);
+                        }}
+                      />
+                    </div>
+                    <div className="description">
+                      <p className="ml-1 text-left">Description</p>
+                      <input
+                        className="p-2 border-1 rounded-3xl w-full"
+                        type="text"
+                        placeholder="text"
+                        onChange={(e) => {
+                          setDescription(e.target.value);
+                        }}
+                      />
+                    </div>
+                    <div className="link">
+                      <p className="ml-1 text-left">Link</p>
+                      <input
+                        className="p-2 border-1 rounded-3xl w-full"
+                        type="text"
+                        placeholder="text"
+                        onChange={(e) => {
+                          setLinkp(e.target.value);
+                        }}
+                      />
+                    </div>
+                    <button type="submit">Submit</button>
+                  </form>
+                </DialogContent>
+              </Dialog>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default Posts;
